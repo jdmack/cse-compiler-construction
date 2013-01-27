804e6222c94da858cedc91fdbcba0ee0a82bc1fe
James Mack <jdmack@ucsd.edu>
2013-01-27 18:43:03 +0000

    Rewrote Operator class

--- Operator/Operator.java
+++ Operator/Operator.java
@@ -4,18 +4,33 @@
 
 abstract class Operator
 {
-    //----------------------------------------------------------------
-    //
-    //----------------------------------------------------------------
+    // class members
+    private String m_opName;
+
+
+    // constructors
     public 
-    Operator ()
+    Operator(String strName)
     {
+        setName(strName);
     }
 
+    // methods
+    public String
+    getName()
+    {
+        return m_opName;
+    }
 
-    //----------------------------------------------------------------
-    //
-    //----------------------------------------------------------------
+    public void
+    setName(String str)
+    {
+        m_typeName = str;
+    }
 
-
+    public STO 
+    checkOperands(STO op1, STO op2)
+    {
+        return (new ErrorSTO("Operator: abstract class")); 
+    }
 }
