21a232b0083a36b8a1f0f2b30804c31bc82ea178
James Mack <jdmack@ucsd.edu>
2013-01-27 21:04:56 +0000

    Pulled Check #1 code for Mod out to Operator class

--- Operator/ModOp.java
+++ Operator/ModOp.java
@@ -19,6 +19,26 @@ class ModOp extends ArithmeticOp
     public STO
     checkOperands (STO op1, STO op2)
     {
-        return (new ErrorSTO("ModOp.checkOperands()"));
+        STO newSTO;
+
+        // Check #1 - Modulus - both int
+        // Check left operand to be int
+        if((!operand1.getType().isInt()))
+        {
+            m_nNumErrors++;
+            m_errors.print (Formatter.toString(ErrorMsg.error1w_Expr, operand1.getType().getName(), op, "int"));    
+            return (new ErrorSTO (operand1.getName()));
+        }
+        // Check right operand to be int
+        else if((!operand2.getType().isInt()))
+        {
+            m_nNumErrors++;
+            m_errors.print (Formatter.toString(ErrorMsg.error1w_Expr, operand2.getType().getName(), op, "int"));    
+            return (new ErrorSTO (operand2.getName()));
+        }
+
+        newSTO = new ExprSTO("DoBinaryOp Result", new IntType());
+
+        return newSTO;
     }
 }
