2d3b543ae73389fed90cb6a6a4425ae653ceb3b0
James Mack <jdmack@ucsd.edu>
2013-01-28 22:48:26 +0000

    Check #1e - Forgot to commit OR AND

--- Operator/BooleanOp.java
+++ Operator/BooleanOp.java
@@ -19,6 +19,23 @@ class BooleanOp extends BinaryOp
     public STO
     checkOperands(STO operand1, STO operand2)
     {
-        return (new ErrorSTO("BooleanOp.checkOperands()"));
+        STO resultSTO;
+
+        // Check #1 - Or, And - Both operands bool
+        // Check operand1
+        if(!operand1.getType().isBool())
+        {
+            return (new ErrorSTO(Formatter.toString(ErrorMsg.error1w_Expr, operand1.getType().getName(), this.getName(), "bool"));
+        }
+        // Check operand2
+        if(!operand2.getType().isBool())
+        {
+            return (new ErrorSTO(Formatter.toString(ErrorMsg.error1w_Expr, operand2.getType().getName(), this.getName(), "bool"));
+        }
+
+        resultSTO = new ExprSTO("NEqualToOp.checkOperands() Result", new BoolType());
+
+        return resultSTO;
+
     }
 }
