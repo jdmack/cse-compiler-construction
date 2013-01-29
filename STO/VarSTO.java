f479263f0f84395e78d1a531dc79269e1bff34bf
James Mack <jdmack@ucsd.edu>
2013-01-28 21:19:16 -0800

    Fix typo in Var

--- STO/VarSTO.java
+++ STO/VarSTO.java
@@ -13,14 +13,18 @@ class VarSTO extends STO
         super(strName);
         // You may want to change the isModifiable and isAddressable 
         // fields as necessary
+        setIsAddressable(true);
+        setIsModifiable(true);
     }
 
     public 
-    VarSTO(String strName, Type typ)
+    VarSTO(String strName, Type type)
     {
         super(strName, type);
         // You may want to change the isModifiable and isAddressable 
         // fields as necessary
+        setIsAddressable(true);
+        setIsModifiable(true);
     }
 
     //---------------------------------------------------------------------
