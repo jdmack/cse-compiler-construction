56e9d582e7762731e6ca88092d8a3420fb12c4b1
James Mack <jdmack@ucsd.edu>
2013-01-29 02:37:56 +0000

    Fix possible uninistialized branch

--- Operator/IncOp.java
+++ Operator/IncOp.java
@@ -42,6 +42,10 @@ class IncOp extends UnaryOp
         {
             resultSTO = new ExprSTO("IncOp.checkOperand() Result", new FloatType());
         }
+        else
+        {
+            return (new ErrorSTO("This will never happen, making compiler happy"));
+        }
 
         return resultSTO;
     }
