0828bebd94f6f29d2bca159fc27fd50fad34be82
James Mack <jdmack@ucsd.edu>
2013-01-29 01:36:45 +0000

    Add constants and additional constructors to NullPtrType class

--- Type/NullPtrType.java
+++ Type/NullPtrType.java
@@ -6,8 +6,26 @@
 class NullPtrType extends PointerType
 {
     //---------------------------------------------------------------------
+    //      Constants
+    //---------------------------------------------------------------------
+    private static final String NULLPTR_NAME = "nullptr";
+    private static final int NULLPTR_SIZE    = "4";
+
+    //---------------------------------------------------------------------
     //      Constructors
     //---------------------------------------------------------------------
+    public 
+    NullPtrType ()
+    {
+        super(NULLPTR_NAME, NULLPTR_SIZE);
+    }
+
+    public 
+    NullPtrType (String strName)
+    {
+        super(strName, NULLPTR_SIZE);
+    }
+
     public 
     NullPtrType (String strName, int size)
     {
