a10a2de25de1556fe8edd7e96038eaa5aa7d8deb
James Mack <jdmack@ucsd.edu>
2013-02-01 00:15:05 -0800

    Fix misnamed thingies

--- Type/VoidType.java
+++ Type/VoidType.java
@@ -4,8 +4,26 @@
 class VoidType extends Type
 {
     //---------------------------------------------------------------------
+    //      Constants
+    //---------------------------------------------------------------------
+    private static final String VOID_NAME = "void";
+    private static final int VOID_SIZE    = 0;
+
+    //---------------------------------------------------------------------
     //      Constructors
     //---------------------------------------------------------------------
+    public
+    VoidType()
+    {
+        this(VOID_NAME, VOID_SIZE);
+    }
+
+    public
+    VoidType(String strName)
+    {
+        this(strName, VOID_SIZE);
+    }
+
     public 
     VoidType (String strName, int size)
     {
