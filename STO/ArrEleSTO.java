9ccf431ccad40930de71806a65cfe9f6afd87876
JunAbbott <jun.abbott@gmail.com>
2013-02-03 17:45:09 -0800

    Added ArrEleSTO.

---
+++ STO/ArrEleSTO.java
@@ -0,0 +1,18 @@
+import java.util.Vector;
+
+
+public class ArrEleSTO extends STO{
+
+	private Vector<STO> exprs;
+	
+	public ArrEleSTO(Vector<STO> exprs) 
+	{
+		super("ArrEleSTO", new VoidType(), false);
+		this.exprs = exprs;
+	}
+	
+	public Vector<STO> getArrayElements()
+	{
+		return exprs;
+	}
+}
