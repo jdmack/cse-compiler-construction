a0cd31468e626812ced5958e9b152614393231a8
James Mack <jdmack@ucsd.edu>
2013-03-03 14:26:29 -0800

    Fix struct again

--- Type/StructType.java
+++ Type/StructType.java
@@ -42,6 +42,6 @@ class StructType extends CompositeType
     
     public boolean isEquivalent(Type type)
     {
-        return (m_typeName == type.getName());
+        return (this.getName() == type.getName());
     }
 }
