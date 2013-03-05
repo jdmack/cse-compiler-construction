1e53dcf2b01ebf2bb9d71adb7a4e5c6da3cc4a47
James Mack <jdmack@ucsd.edu>
2013-03-04 23:06:54 -0800

    Remove comments

--- STO/ExprSTO.java
+++ STO/ExprSTO.java
@@ -10,20 +10,11 @@ class ExprSTO extends STO
     public ExprSTO(String strName)
     {
         super(strName, null, false);
-        // You may want to change the isModifiable and isAddressable
-        // fields as necessary
-
-        // Don't need to because we want false and that's the initial value
-
     }
 
     public ExprSTO(String strName, Type typ)
     {
         super(strName, typ, false);
-        // You may want to change the isModifiable and isAddressable
-        // fields as necessary
-
-        // Don't need to because we want false and that's the initial value
     }
 
     //---------------------------------------------------------------------
