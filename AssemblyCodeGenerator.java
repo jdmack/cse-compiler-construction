3b0a7d9a315020067358f070bfc307398408b132
James Mack <jdmack@ucsd.edu>
2013-03-06 00:17:07 -0800

    More changes

--- AssemblyCodeGenerator.java
+++ AssemblyCodeGenerator.java
@@ -5,6 +5,8 @@ import java.util.Date;
 
 public class AssemblyCodeGenerator {
 
+    // Compiler identifier
+    private final String COMPILER_IDENT = "WRC 1.0";
     // level of indentation for current code
     private int indent_level = 0;
     
@@ -68,35 +70,19 @@ public class AssemblyCodeGenerator {
             System.exit(1);
         }
     }
-    
-    
 
-    
-    // 9) This signature may look foreign to you.  What is says is that we have 
-    //    public method named writeAssembly which takes as parameters a String
-    //    followed by 1 or more strings.  This construct is called "VarArgs" and
-    //    is a Java 1.5 feature.  This allows you to write one method which can
-    //    be applied to any number of parameters.  This method simply takes in 
-    //    a template and all the strings that will be substituted into the 
-    //    template.  When you are actually in the method, the parameter 
-    //    String ... params will be an array of strings.
+    //-------------------------------------------------------------------------
+    //      writeAssembly
+    //-------------------------------------------------------------------------
+    // params = String []
     public void writeAssembly(String template, String ... params) {
         StringBuilder asStmt = new StringBuilder();
         
-        // 10) This is where we use our indent_level.  We will indent indent_level levels
-        //     of indentation.  That is an awkward sentence isn't it!  StringBuilder is
-        //     an efficient class to build strings from concatentations.  If your 
-        //     concatenations span multiple lines of code, using a StringBuilder can
-        //     offer signifigant performance when compared to using the + operator.
-        //     This topic can get fairly detailed, send me an email or come talk to me
-        //     in the lab for more details.
+        // Indent current line
         for (int i=0; i < indent_level; i++) {
-            asStmt.append(SparcInstr.SEPARATOR);
+            asStmt.append(SparcInstr.INDENTOR);
         }
         
-        // 11) Here we are writing the message to file, notice we are using the 
-        //     String.format method which takes a printf like format string followed
-        //     by an array of Objects which are the parameters to the format string.
         asStmt.append(String.format(template, (Object[])params));
         
         try {
@@ -108,10 +94,10 @@ public class AssemblyCodeGenerator {
         }
     }
     
-    // 12) Main is just a small demo that will create a tiny assembly file in the
-    //     current directory called "output.s".  This file doesn't compile and is
-    //     not meant to.
-    public static void main(String args[]) {
+    //-------------------------------------------------------------------------
+    //      example
+    //-------------------------------------------------------------------------
+    public void example() {
         AssemblyCodeGenerator myAsWriter = new AssemblyCodeGenerator("output.s");
 
         myAsWriter.increaseIndent();
@@ -137,7 +123,7 @@ public class AssemblyCodeGenerator {
     //-------------------------------------------------------------------------
     public void DoProgramStart(String filename)
     {
-
+         
 
     }
 
