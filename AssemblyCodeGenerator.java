633744a5ca375c3559848bd7e98fb51d239d98f5
JunAbbott <jun.abbott@gmail.com>
2013-03-09 17:46:40 -0800

    Fixed typo for string cout

--- AssemblyCodeGenerator.java
+++ AssemblyCodeGenerator.java
@@ -400,9 +400,9 @@ public class AssemblyCodeGenerator {
             // .align 4
         	writeAssembly(SparcInstr.ONE_PARAM, SparcInstr.ALIGN_DIR, "4");
         	// set _strFmt, %o0
-        	 writeAssembly(SparcInstr.TWO_PARAM, SparcInstr.SET_OP, ".strFmt", "%l0");
+        	 writeAssembly(SparcInstr.TWO_PARAM, SparcInstr.SET_OP, SparcInstr.STRFMT, "%l0");
         	// set str_(str_count), %o1
-        	writeAssembly(SparcInstr.TWO_PARAM, SparcInstr.SET_OP, "str_"+str_count, "%l1");
+        	writeAssembly(SparcInstr.TWO_PARAM, SparcInstr.SET_OP, ".str_"+str_count, "%l1");
             // call printf
         	writeAssembly(SparcInstr.ONE_PARAM, SparcInstr.CALL_OP, SparcInstr.PRINTF);
             // nop
