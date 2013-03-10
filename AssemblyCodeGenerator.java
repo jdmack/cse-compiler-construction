d86e59457c3298e10342971834bcb949d31b857f
James Mack <jdmack@ucsd.edu>
2013-03-09 22:50:26 -0800

    Still fixing

--- AssemblyCodeGenerator.java
+++ AssemblyCodeGenerator.java
@@ -531,7 +531,7 @@ public class AssemblyCodeGenerator {
         sto.store(SparcInstr.REG_FRAME, offset);
 
         // Initialize to 0, mostly for testing purposed
-        writeAssembly(SparcInstr.TWO_PARAM, SparcInstr.SET_OP, SparcInstr.REG_GLOBAL0, SparcInstr.REG_LOCAL0);
+        writeAssembly(SparcInstr.TWO_PARAM, SparcInstr.MOV_OP, SparcInstr.REG_GLOBAL0, SparcInstr.REG_LOCAL0);
         writeAssembly(SparcInstr.TWO_PARAM, SparcInstr.STORE_OP, SparcInstr.REG_LOCAL0, bracket(sto.load()));
         
         // For float, check DI6 Page on "What about float?"
