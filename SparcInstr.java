5ee678328bc4f9fd6a1b6e6b6efd530125f6db4f
James Mack <jdmack@ucsd.edu>
2013-03-12 05:21:56 -0700

    Add float compare and branches

--- SparcInstr.java
+++ SparcInstr.java
@@ -191,18 +191,27 @@ class SparcInstr
     //-----------------------------------------------------------------------------
     //      Compare
     //-----------------------------------------------------------------------------
-    public static final String CMP_OP = "cmp";
+    public static final String CMP_OP   = "cmp";
+    public static final String FCMPS_OP = "fcmps";
 
     //-----------------------------------------------------------------------------
     //      Branch
     //-----------------------------------------------------------------------------
-    public static final String BG_OP  = "bg";
-    public static final String BGE_OP = "bge";
-    public static final String BL_OP  = "bl";
-    public static final String BE_OP  = "be";
-    public static final String BNE_OP = "bne";
-    public static final String BA_OP  = "ba";
-    public static final String BN_OP  = "bn";
+    public static final String BG_OP   = "bg";
+    public static final String BGE_OP  = "bge";
+    public static final String BL_OP   = "bl";
+    public static final String BE_OP   = "be";
+    public static final String BNE_OP  = "bne";
+    public static final String BA_OP   = "ba";
+    public static final String BN_OP   = "bn";
+
+    public static final String FBG_OP  = "fbg";
+    public static final String FBGE_OP = "fbge";
+    public static final String FBL_OP  = "fbl";
+    public static final String FBE_OP  = "fbe";
+    public static final String FBNE_OP = "fbne";
+    public static final String FBA_OP  = "fba";
+    public static final String FBN_OP  = "fbn";
     
     //-----------------------------------------------------------------------------
     //      Call
